60365cd025fe07c38d41ce5615b6436eba05136b
3mmaRand <emma.rand@york.ac.uk>
2020-10-16 17:54:32 +0100

    week 4 independent study completed.

---
+++ hello_git.R
@@ -0,0 +1,19 @@
+# Introduce your yourself to git
+# 
+# 
+# You are going to need your GitHub username and password later
+
+# User name
+#  does not have to be your GitHub user name but can be
+#  consider using your actual name - your commits will
+#  be labelled with this user name so make it informative
+#  to others and future you.
+# 
+# Email
+# This must be the email associated with your 
+# GitHub account.
+
+
+
+usethis::use_git_config(user.name = "Emma Rand",
+                        user.email = "emma.rand@york.ac.uk")
